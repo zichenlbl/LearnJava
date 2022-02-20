06e64934f2731505c7874246fd95c76cdc4d508f
zichen <zichen1016@qq.com>
2022-02-20 13:20:08 +0800

    文件字节输入输出流复制文件

---
+++ IO流/字节输入输出流-复制图片/src/CopyFile.java
@@ -0,0 +1,55 @@
+import java.io.*;
+
+/**
+ * 复制文件
+ */
+public class CopyFile {
+
+    public static void main(String[] args) {
+        // 文件路径
+        String courseFile = "";
+        try {
+            // 获取项目路径 D:\LearnJava\IO流\demo\src\logo.png
+            courseFile = new File("").getCanonicalPath();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String fromFilePath = courseFile + "\\src\\logo.png";
+        String toFilePath = courseFile +"/src/logo1.png";
+        FileInputStream fileInputStream = null;
+        FileOutputStream fileOutputStream = null;
+        try {
+            // 字节输入流
+            fileInputStream = new FileInputStream(fromFilePath);
+            // 字节输出流
+            fileOutputStream = new FileOutputStream(toFilePath);
+            // 一次最大读取的长度
+            byte[] buffer = new byte[1024];
+            int readLen = 0;
+            while ((readLen = fileInputStream.read(buffer)) != -1) {
+                // 读一次，写一次 写入读取的长度
+                fileOutputStream.write(buffer, 0, readLen);
+            }
+            System.out.println("ok!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 后用的先关
+            if (fileOutputStream != null) {
+                try {
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
